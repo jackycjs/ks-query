443b3b954c2186afb0cb0be7ad316e27c6834e87
Jacky Chen <jacky_cjs@163.com>
2017-11-16 14:59:33 +0800

    update

--- ksQuery.js
+++ ksQuery.js
@@ -1,27 +1,65 @@
-;(function(window) {
+/*
+ * @Author: Jacky Chen
+ * @Date: 2017-09-26 14:10:09 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2017-09-26 15:57:53
+ */
 
-	var DOC = window.document,
-		toString = Object.prototype.toString,
-		strArray = "[object Array]",
-		strFunction = "[object Function]",
-		strObject = "[object Object]",
+;( function(global, factory) {
+
+	if (typeof module === 'object' && typeof module.exports === 'ojbect') {
+		module.exports = global.document ?
+			factory(global, true) : 
+			function(w) {
+				if (!w.document) {
+					throw new Error('ksQuery requires a window with a document')
+				}
+				return factory(w);
+			}
+	} else {
+		factory(global);
+	}
+	
+})(typeof window !== 'undefined' ? window : this, function(window) {
+	
+	var document = window.document;
+
+	var arr = [];
+	var slice = arr.slice;
+	var concat = arr.concat;
+	var push = arr.push;
+	var indexOf = arr.indexOf;
+
+	var class2type = {};
+	var toString = class2type.toString;
+	var hasOwn = class2type.hasOwnProperty;
+
+
+
+
+
+
+	var	toString = Object.prototype.toString,
+		strArray = '[object Array]',
+		strFunction = '[object Function]',
+		strObject = '[object Object]',
 		trimExpr = /^(\s|\u00A0)+|(\s|\u00A0)+$/g; //  简单版: /(^\s*)|(\s*$)/g    jquery版: /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g
 		
-	var aQuery = function(selector) {
-	    return new aQuery.fn.init(selector);
+	var ksQuery = function(selector) {
+			return new ksQuery.fn.init(selector);
 	};
 
-	aQuery.fn = aQuery.prototype = {
-	    name: 'Jacky',
-		init: function(selector) {
+	ksQuery.fn = ksQuery.prototype = {
+			name: 'Jacky',
+			init: function(selector) {
 			this.selector = selector;
 			sizzle(this, selector);
 			return this;
 		},
-		constructor: aQuery,
+		constructor: ksQuery,
 
 		each: function(callback) {
-			return aQuery.each(this, callback);
+			return ksQuery.each(this, callback);
 		},
 
 		append: function() {
@@ -35,7 +73,7 @@
 		},
 		
 		parent: function() {
-			return dir.call(this, "parentNode", 0, selector);
+			return dir.call(this, 'parentNode', 0, selector);
 		},
 		parents: function() {
 			
@@ -44,19 +82,19 @@
 			
 		}
 	};
-	
+
 	var sizzle =  function(newA, selector){
 		var domlist = document.querySelectorAll(selector);
 		Array.prototype.push.apply(newA, domlist);
 		return newA;
 	};
 
-	aQuery.fn.init.prototype = aQuery.fn;
+	ksQuery.fn.init.prototype = ksQuery.fn;
 
-	aQuery.extend = aQuery.fn.extend = function() {
+	ksQuery.extend = ksQuery.fn.extend = function() {
 		var options,
 			target = arguments[0] || {},
-		 	i = 1,
+			i = 1,
 			length = arguments.length;
 
 		//只有一个参数，就是对jQuery自身的扩展处理
@@ -75,15 +113,16 @@
 		return target;
 	};
 
-	aQuery.extend({
+	ksQuery.extend({
 
 		each: function(obj, callback){
 			var value,
 				i = 0,
 				length = obj.length,
-				isArray = isArray( obj );
+				isArray = ksQuery.isArray( obj );
 
 			if ( isArray ) {
+				console.log('in');
 				for ( ; i < length; i++ ) {
 					value = callback.apply( obj[ i ] );
 
@@ -104,23 +143,31 @@
 			return obj;
 		},
 
-		isArray: function(obj){
-			return toString.call(obj) === strArray;  // strArray = "[object Array]",
-		},
+		isArray: Array.isArray,
 
 		isFunction: function(obj, type) {
-			return toString.call(obj) === strFunction;  // strFunction = "[object Function]"
+			return toString.call(obj) === strFunction;  // strFunction = '[object Function]'
 		},
 
 		isObject: function(obj){
-			return typeof obj == "object" && toString.call(obj) === strObject;  // strObject = "[object Object]"
+			return typeof obj == 'object' && toString.call(obj) === strObject;  // strObject = '[object Object]'
 		},
 
 		trim: function(str){
 			if(String.prototype.trim) {
 				return str.trim();
 			}
-			return str.replace(trimExpr, "");
+			return str.replace(trimExpr, '');
+		},
+
+		type: function(obj) {
+			if (obj == null) {
+				return obj + '';
+			}
+
+			return typeof obj === 'object' || typeof obj === 'function' ?
+				class2type[toString.call(obj)] || 'object' :
+				typeof obj;
 		},
 
 		/* Demo for myself */
@@ -137,14 +184,19 @@
 		}
 	});
 
-	function dir(elem, dir, until){
+	// ksQuery.each('Boolean Number String Function Array Date RegExp Object Error Symbol'.split(' '), function(i, name) {
+	// 	class2type['[object ' + name + ']'] = name.toLowerCase();
+	// })
+
+	function dir(elem, dir, until) {
 		var arr = [];
 		
 		
 		return arr;
 	}
 
-	aQuery.fn.extend({
+
+	ksQuery.fn.extend({
 		/*setName: function(myName) {
 			this.myName = myName;
 			return this;
@@ -154,9 +206,17 @@
 			return this;
 		}*/
 	});
+	
 
-	window.$$ = window.aQuery = aQuery;
+	if (typeof define === 'function' && define.amd) {
+		define('ksQuery', [], function() {
+			return ksQuery;
+		});
+	}
 
-})(typeof window !== "undefined" ? window : this);
+	window.ksQuery = window.$ = ksQuery;
+
+	return ksQuery;
+});
 
 
